db8746bb71f9e4a084942cfe6bec721889f9a7bf
Desi Rahmawati <rahmawatidesi730@gmail.com>
2021-03-18 15:42:36 +0700

    perulangan commit

--- Dasar Dart/dasar_dart/bin/perulangan.dart
+++ Dasar Dart/dasar_dart/bin/perulangan.dart
@@ -1,8 +1,12 @@
 import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
 import 'dart:io';
 
-main(List<String> args) {
-  for (int counter = 0; counter < 10; counter = counter + 1){
+main(List<String> arguments) {
+//   for (int counter = 0; counter < 10; counter = counter + 1){
+//     print("hallo");
+//   }
+// }
+  for (int counter = 5; counter >= 1; counter = counter - 1){
     print("hallo");
   }
 }
