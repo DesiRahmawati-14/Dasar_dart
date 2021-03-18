84a4fa89e4bd99657ee16f021206bb05fc1a40b0
Desi Rahmawati <rahmawatidesi730@gmail.com>
2021-03-18 11:35:23 +0700

    fungsi commit

--- Dasar Dart/dasar_dart/bin/fungsi.dart
+++ Dasar Dart/dasar_dart/bin/fungsi.dart
@@ -1,12 +1,21 @@
 import 'dart:io';
 
 double luas_segiempat(double panjang, double lebar){
-  double hasil;
-  hasil = panjang * lebar;
-  return hasil;
+  // double hasil;
+  // hasil = panjang * lebar;
+  // return hasil;
+  return panjang * lebar;
+
 }
 
+// void sapa_penonton(){
+//   print("hallo penonton!");
+// }
+
 main (List<String> arguments){
+
+// sapa_penonton();
+
 double p, l ,luas;
 
    p = double.tryParse(stdin.readLineSync());
@@ -14,7 +23,7 @@ double p, l ,luas;
 
   luas = luas_segiempat(p, l);
 
-  print(luas);
+  // print(luas);
 
-      // print (luas_segiempat(p, l));
+      print (luas_segiempat(p, l));
 }
